2a376f8338f4536ef1c54210d30f1778c412e569
Shakaet <abdshakaet@gmail.com>
2024-09-18 19:04:34 +0600

    add

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// console.log("login")
+
+document.getElementById("btn").addEventListener("click",function(event){
+    
+
+    event.preventDefault();  // eta dara website reload howa ke prevent kore
+    console.log("hello")
+})
