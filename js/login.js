7496d2728ed8fbe9b735f72d437ec24eaaac1e2e
Shakaet <abdshakaet@gmail.com>
2024-09-18 19:28:55 +0600

    add

--- js/login.js
+++ js/login.js
@@ -1,8 +1,33 @@
 // console.log("login")
 
+
+//step1: add event
 document.getElementById("btn").addEventListener("click",function(event){
     
-
+     // step2: prevent default behavior
     event.preventDefault();  // eta dara website reload howa ke prevent kore
-    console.log("hello")
+    // console.log("hello")
+
+    //get the text from input field and pin number
+
+    let input_text=document.getElementById("text").value
+    // console.log(input_text)
+
+    let input_pin=document.getElementById("pin").value
+    // console.log(input_pin)
+
+    if(input_text==="01920293858" && input_pin==="1234asdf"){
+
+        console.log("You Are Succesfully login")
+
+        window.location.href="/home.html"
+
+
+    }
+    else{
+        alert("You Are give wrong number ot pin")
+    }
+
+
+
 })
